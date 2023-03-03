9fc38c5b9a582c61482bf9ac43c4ebdbe9139e24
syedalihader <126227473+syedalihader@users.noreply.github.com>
2023-03-03 11:59:51 +0500

    lab2
    lab2

---
+++ lab2.py
@@ -0,0 +1,24 @@
+greeting = "2009 Microsoft Corporation. All rights reserved hellow world ******^$#%$#$#"
+print(greeting)
+
+ 
+name= "syed ali haider"
+lastname = "akber ali shah"
+fathername ="akber"
+
+print("name:", name)
+print("lastname:", lastname)
+print("fathername:", fathername)
+a= "                                                              *******  my profile  ***************                                 "
+name= "ali"
+lastname="haider"
+Department="ist"
+age=(14)
+
+qulification="phd masters in python"
+print(a)
+print(name)
+print(lastname)
+print(Department)
+print(age)
+print(qulification)
